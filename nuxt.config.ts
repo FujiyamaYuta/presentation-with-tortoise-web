13aa0ce801cd82fe940dcd6f3527cfcfdf4093f4
YutaFujiwara <yuta.fujiwara1029@gmail.com>
2025-08-16 07:53:29 +0900

    title・description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,18 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/fonts', '@nuxt/icon', '@pinia/nuxt', '@nuxtjs/tailwindcss'],
-  css: ['~/assets/css/tailwind.css']
+  css: ['~/assets/css/tailwind.css'],
+  app: {
+    head: {
+      title: 'Tortoise',
+      meta: [
+        { 
+          name: 'description', 
+          content: 'このサービスはプレゼンを時間通りに進めるためのツールです。うさぎ（🐰 スライドの進捗）とカメ（🐢 時間）でそれぞれが進み、現時点のスライドの進捗を可視化することができます。Rabbitというツールをオマージュしており、Nuxt3で作っています。'
+        },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { charset: 'utf-8' }
+      ]
+    }
+  }
 })

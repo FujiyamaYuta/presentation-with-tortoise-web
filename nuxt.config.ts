44a5a929078ac9e5a20468008c90904701d5636e
YutaFujiwara <yuta.fujiwara1029@gmail.com>
2025-08-17 07:22:17 +0900

    ogimageを追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,15 @@ export default defineNuxtConfig({
           content: 'このサービスはプレゼンを時間通りに進めるためのツールです。うさぎ（🐰 スライドの進捗）とカメ（🐢 時間）でそれぞれが進み、現時点のスライドの進捗を可視化することができます。Rabbitというツールをオマージュしており、Nuxt3で作っています。'
         },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { charset: 'utf-8' }
+        { charset: 'utf-8' },
+        { property: 'og:title', content: 'Tortoise' },
+        { property: 'og:description', content: 'このサービスはプレゼンを時間通りに進めるためのツールです。うさぎ（🐰 スライドの進捗）とカメ（🐢 時間）でそれぞれが進み、現時点のスライドの進捗を可視化することができます。' },
+        { property: 'og:image', content: '/og.png' },
+        { property: 'og:type', content: 'website' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: 'Tortoise' },
+        { name: 'twitter:description', content: 'このサービスはプレゼンを時間通りに進めるためのツールです。うさぎ（🐰 スライドの進捗）とカメ（🐢 時間）でそれぞれが進み、現時点のスライドの進捗を可視化することができます。' },
+        { name: 'twitter:image', content: '/og.png' }
       ]
     }
   }
